4305a2ebddcd358f89ef4a878f0f8e7018c6180b
linzq1439 <111324968+linzq1439@users.noreply.github.com>
2022-09-07 20:05:21 +0800

    Initial Commit

---
+++ Shared/testOfGitApp.swift
@@ -0,0 +1,17 @@
+//
+//  testOfGitApp.swift
+//  Shared
+//
+//  Created by lin on 2022/9/7.
+//
+
+import SwiftUI
+
+@main
+struct testOfGitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
